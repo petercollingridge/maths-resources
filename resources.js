8fe47fed5af3b93a6c024b3cac4dcd943a312ab8
Peter Collingridge <peter.collingridge@nanoporetech.com>
2020-03-29 11:52:12 +0100

    Create option for hexagonal grid

--- resources.js
+++ resources.js
@@ -1,7 +1,11 @@
+var deg60 = Math.PI / 3;
+var SQRT3 = Math.sqrt(3);
+
 var vm = new Vue({
     el: '#math-resources',
     data: {
        title: 'Triangle',
+       tileShape: 'square',
        tileWidth: 30,
        tileHeight: 30,
        gridSize: 16,
@@ -26,6 +30,18 @@ var vm = new Vue({
         },
         getY(y) {
             return this.tileHeight * (y - 1) + 1;
+        },
+        getHexagon(x, y, size) {
+            var r = size * SQRT3 / 3;
+            var cx = this.getX(x, y) + r - 2;
+            var cy = this.getY(y) * r * 1.5 / this.tileHeight + r;
+            var d = "";
+            for (var i = 0; i < 6; i++) {
+                var px = cx + r * Math.sin(i * deg60);
+                var py = cy + r * Math.cos(i * deg60);
+                d += (i ? 'L' : 'M') + px + ' ' + py;
+            }
+            return d + 'z';
         }
     }
 });
