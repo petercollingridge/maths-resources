604ee8ca8c62ef074b7b12405ad7f6b64abda4cb
Peter Collingridge <peter.collingridge@nanoporetech.com>
2020-12-29 15:58:17 +0000

    Updates

---
+++ precache-manifest.72fee01726c59d614f8336efacbdf064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d6b715f7988dd40f0e674612c9d809",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91231371052b6194804b",
+    "url": "/static/css/main.0da89c60.chunk.css"
+  },
+  {
+    "revision": "e66cb9e51f4879d5fb0c",
+    "url": "/static/js/2.d59e2ddb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d59e2ddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91231371052b6194804b",
+    "url": "/static/js/main.f7b9159f.chunk.js"
+  },
+  {
+    "revision": "af9652439d13fcfff263",
+    "url": "/static/js/runtime-main.c43e86a8.js"
+  }
+]);
